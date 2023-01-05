4869dc3b91e2d2c8e7298d54d6b2eb6fe7f233a2
John McMaster <johndmcmaster@gmail.com>
2023-01-05 14:17:34 -0800

    apply format.sh
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- src/gstplugin.c
+++ src/gstplugin.c
@@ -12,23 +12,20 @@
 #include "gsttoupcamsrc.h"
 
 #define GST_CAT_DEFAULT gst_gsttoupcam_debug
-GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
+GST_DEBUG_CATEGORY_STATIC(GST_CAT_DEFAULT);
 
 #include <stdio.h>
 
-static gboolean
-plugin_init (GstPlugin * plugin)
+static gboolean plugin_init(GstPlugin * plugin)
 {
-printf("registering...\n");
-  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, "toupcamsrc", 0,
-      "debug category for ToupCam elements");
+    printf("registering...\n");
+    GST_DEBUG_CATEGORY_INIT(GST_CAT_DEFAULT, "toupcamsrc", 0,
+                            "debug category for ToupCam elements");
 
-  return gst_element_register (plugin, "toupcamsrc", GST_RANK_NONE,
-          GST_TYPE_TOUPCAM_SRC);
+    return gst_element_register(plugin, "toupcamsrc", GST_RANK_NONE,
+                                GST_TYPE_TOUPCAM_SRC);
 }
 
-GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
-    GST_VERSION_MINOR,
-    toupcamsrc,
-    "ToupCam camera source element.",
-    plugin_init, VERSION, "BSD", PACKAGE_NAME, "http://www.touptek.com")
+GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, toupcamsrc,
+                  "ToupCam camera source element.", plugin_init, VERSION,
+                  "BSD", PACKAGE_NAME, "http://www.touptek.com")
--- src/gsttoupcamsrc.h
+++ src/gsttoupcamsrc.h
@@ -24,13 +24,13 @@ TODO: add tucsen rebrand
 
 */
 
-//XXX: SDK_BRANDING
+// XXX: SDK_BRANDING
 #define CAMSDK_TOUPTEK
-//Amscope
+// Amscope
 //#define CAMSDK_AMCAM
 //? "MIView", Came with 25 MP camera
 //#define CAMSDK_NNCAM
-//Swift
+// Swift
 //#define CAMSDK_SWIFTCAM
 
 /*
@@ -38,62 +38,62 @@ Ex: toupcamsdk.h: Version: 53.21522.20221011 => 53
 
 In general we support only the latest version
 But some backwards compatibility is ok when easy / convenient
-Ex: we have a non-public SDK release but also want to support latest public release
+Ex: we have a non-public SDK release but also want to support latest public
+release
 */
 #define CAMSDK_VERSION 53
 
 #if defined(CAMSDK_TOUPTEK)
-#include  <toupcam.h>
-#define camsdk(x) Toupcam ## x
-#define camsdk_(x) Toupcam_ ## x
-#define CAMSDK_(x) TOUPCAM_ ## x
+#include <toupcam.h>
+#define camsdk(x) Toupcam##x
+#define camsdk_(x) Toupcam_##x
+#define CAMSDK_(x) TOUPCAM_##x
 #define CAMSDK_HANDLE HToupcam
 #elif defined(CAMSDK_AMCAM)
 #include <amcam.h>
-#define camsdk(x) Amcam ## x
-#define camsdk_(x) Amcam_ ## x
-#define CAMSDK_(x) AMCAM_ ## x
+#define camsdk(x) Amcam##x
+#define camsdk_(x) Amcam_##x
+#define CAMSDK_(x) AMCAM_##x
 #elif defined(CAMSDK_NNCAM)
 #include <nncam.h>
-#define camsdk(x) Nncam ## x
-#define camsdk_(x) Nncam_ ## x
-#define CAMSDK_(x) NNCAM_ ## x
+#define camsdk(x) Nncam##x
+#define camsdk_(x) Nncam_##x
+#define CAMSDK_(x) NNCAM_##x
 #define CAMSDK_HANDLE HNncam
 #elif defined(CAMSDK_SWIFTCAM)
 #include <swiftcam.h>
-#define camsdk(x) Swiftcam ## x
-#define camsdk_(x) Swiftcam_ ## x
-#define CAMSDK_(x) SWIFTCAM_ ## x
+#define camsdk(x) Swiftcam##x
+#define camsdk_(x) Swiftcam_##x
+#define CAMSDK_(x) SWIFTCAM_##x
 #else
 #error Need SDK brand
 #endif
 
-
-
 G_BEGIN_DECLS
-
-#define GST_TYPE_TOUPCAM_SRC   (gst_toupcam_src_get_type())
-#define GST_TOUPCAM_SRC(obj)   (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_TOUPCAM_SRC,GstToupCamSrc))
-#define GST_TOUPCAM_SRC_CLASS(klass)   (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_TOUPCAM_SRC,GstToupCamSrcClass))
-#define GST_IS_TOUPCAM_SRC(obj)   (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_TOUPCAM_SRC))
-#define GST_IS_TOUPCAM_SRC_CLASS(obj)   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_TOUPCAM_SRC))
-
+#define GST_TYPE_TOUPCAM_SRC (gst_toupcam_src_get_type())
+#define GST_TOUPCAM_SRC(obj)                                                   \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_TOUPCAM_SRC, GstToupCamSrc))
+#define GST_TOUPCAM_SRC_CLASS(klass)                                           \
+  (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_TOUPCAM_SRC, GstToupCamSrcClass))
+#define GST_IS_TOUPCAM_SRC(obj)                                                \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_TOUPCAM_SRC))
+#define GST_IS_TOUPCAM_SRC_CLASS(obj)                                          \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_TOUPCAM_SRC))
 typedef struct _GstToupCamSrc GstToupCamSrc;
 typedef struct _GstToupCamSrcClass GstToupCamSrcClass;
 
-struct _GstToupCamSrc
-{
+struct _GstToupCamSrc {
     GstPushSrc base_toupcam_src;
 
     // device
     /*
-    hmm...code diverged?
-    Version: 50.19728.20211022
-    typedef struct ToupcamT { int unused; } *HToupcam, *HToupCam;
-    53.21522.20221011
-    typedef struct Nncam_t { int unused; } *HNncam;
-    */
-    CAMSDK_HANDLE hCam;  // device handle
+       hmm...code diverged?
+       Version: 50.19728.20211022
+       typedef struct ToupcamT { int unused; } *HToupcam, *HToupCam;
+       53.21522.20221011
+       typedef struct Nncam_t { int unused; } *HNncam;
+     */
+    CAMSDK_HANDLE hCam;         // device handle
     gboolean raw;
     gboolean x16;
     gint esize;
@@ -105,7 +105,7 @@ struct _GstToupCamSrc
     gint bytes_per_pix_out;
     gint image_bytes_out;
     gint m_total;
-    gint gst_stride;  // Stride/pitch for the GStreamer buffer
+    gint gst_stride;            // Stride/pitch for the GStreamer buffer
 
     unsigned char *frame_buff;
 
@@ -113,15 +113,15 @@ struct _GstToupCamSrc
     gdouble framerate;
     gdouble maxframerate;
     // library based properties
-    //bool
+    // bool
     int hflip;
-    //bool
+    // bool
     int vflip;
-    //bool
+    // bool
     int auto_exposure;
-    //unsigned
+    // unsigned
     unsigned expotime;
-    //ints
+    // ints
     int hue;
     int saturation;
     int brightness;
@@ -144,13 +144,11 @@ struct _GstToupCamSrc
     GCond cond;
 };
 
-struct _GstToupCamSrcClass
-{
-  GstPushSrcClass base_toupcam_src_class;
+struct _GstToupCamSrcClass {
+    GstPushSrcClass base_toupcam_src_class;
 };
 
-GType gst_toupcam_src_get_type (void);
+GType gst_toupcam_src_get_type(void);
 
 G_END_DECLS
-
 #endif
